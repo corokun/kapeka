0f65c0ba6ef6c13ad4be67341018945df86c171b
Kun <kunrails@gmail.com>
2015-09-14 16:33:25 +0700

    Initial commit

--- lib/kapeka.rb
+++ lib/kapeka.rb
@@ -2,4 +2,23 @@ require "kapeka/version"
 
 module Kapeka
   # Your code goes here...
+  def self.fpb(x, y)
+    if x == 0
+      return y
+    elsif y == 0
+      return x
+    else
+      self.fpb(y, x%y)
+    end
+  end
+
+  def self.kpk(x, y)
+    (x*y)/self.fpb(x,y)
+  end
+end
+
+class Fixnum
+  def kpkthis(a)
+    Kapeka.kpk(self, a)
+  end
 end
